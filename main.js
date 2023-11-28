bfe4ab38dee2323812e61e224ddb2d5e9f797477
Artsiom Marzavin <marzavin@gmail.com>
2023-11-28 18:31:04 +0100

    Changes in BrowserWindow configuration

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ const path = require('node:path');
 
 const createWindow = () => {
     const window = new BrowserWindow({
+        title: 'Colosseum',
         width: 800,
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     });
+
+    window.setMenu(null);
 
     window.loadFile('index.html');
 };
