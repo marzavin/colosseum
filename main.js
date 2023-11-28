301903e885de25c0ea6373260b1c93058a6f80bf
Artsiom Marzavin <marzavin@gmail.com>
2023-11-28 15:56:45 +0100

    Base for new electron app

--- main.js
+++ main.js
@@ -1,38 +1,31 @@
 const { app, BrowserWindow } = require('electron');
-const url = require('url');
-const path = require('path'); 
 
-let win;
+const path = require('node:path');
 
-function createWindow() {
-    win = new BrowserWindow({
-        width: 1024,
-        height: 768
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
 
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'dist/colosseum/index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+    window.loadFile('index.html');
+};
 
-    //win.webContents.openDevTools();
+app.whenReady().then(() => {
+    createWindow()
 
-    win.on('closed', function() {
-        win = null;
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
     });
-}
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', function() {
-    if(process.platform !== 'darwin') {
-        app.quit();
-    }
 });
 
-app.on('activate', function( ){
-    if(win === null) {
-        createWindow();
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
     }
 });
